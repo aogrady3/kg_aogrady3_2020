ad1b5a01d41089916516dd847d64314e6b378696
Andrew O'Grady <andrewogrady3@gmail.com>
2020-03-14 09:41:35 -0400

    main function created

--- main.js
+++ main.js
@@ -1,5 +1,77 @@
 const MaxHeap = require('./maxHeap')
 
+/**
+ * 
+ * @param {*String} stringOne 
+ * @param {*String} stringTwo 
+ * @returns Boolean
+ * 
+ * Main function takes in two strings from command line and prints results
+ */
+
+function isOneToOneMappable(strOne, strTwo) {
+    // console.log(process.argv.slice(2))
+
+    let strOneHash = {}
+    let strTwoHash = {}
+
+    //Can return false right away since one-to-one mapping would not be possible
+    if(strOne.length > strTwo.length) return false
+
+    //Count unique characters in both strings
+    for (let i = 0; i < strOne.length; i++) {
+        let char = strOne[i]
+        if(!strOneHash[char]) strOneHash[char] = 0
+        strOneHash[char]++
+    }
+
+    for (let j = 0; j < strTwo.length; j++) {
+        let char = strTwo[j]
+        if(!strTwoHash[char]) strTwoHash[char] = 0
+        strTwoHash[char]++
+    }
+
+    //Take unique character values and create heap 
+    let strOneValues = Object.values(strOneHash)
+    let strTwoValues = Object.values(strTwoHash)
+
+    
+    let strOneHeap = new MaxHeap(strOneValues)
+    let strTwoHeap = new MaxHeap(strTwoValues)
+
+
+
+    //compare each max heap value subtract them, assign new values, and siftDown()
+    //repeat until all values are 0 or negtaive value is reached
+    while(strOneHeap.peek() !== 0) {
+         valueOne = strOneHeap.peek()
+         valueTwo = strTwoHeap.peek()
+
+         //break and return false
+         if(valueOne > valueTwo) return false
+
+         let difference = valueTwo - valueOne
+
+         //change value will siftDown() the newly changed value 
+         //so heap always reamins valid
+         strOneHeap.changeValue(0, 0)
+         strTwoHeap.changeValue(0, difference)
+
+    }
+
+
+    return true
+
+}
+
+console.log(isOneToOneMappable('haaqqt', 'pootyt'))
+console.log(isOneToOneMappable('pootyt', 'haaqxt'))
+console.log(isOneToOneMappable('foo', 'bar'))
+console.log(isOneToOneMappable('bar', 'foo'))
+console.log(isOneToOneMappable('123', '321'))
+console.log(isOneToOneMappable('aaaqqxlm', 'xxxxtppl'))
+console.log(isOneToOneMappable('xxxxtppl', 'aaaqqxlm'))
+console.log(isOneToOneMappable('aaaqqxlm', 'xxxxtpl'))
 
 
 
